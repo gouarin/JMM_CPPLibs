47f9e688282229c5e2d57136e7d2ee706734827a
Jean-Marie Mirebeau <jean-mariemirebeau@pc93-123.math.u-psud.fr>
2018-06-15 15:07:16 +0200

    removed support for boost python, replaced with pybind 11

--- Output/Output/PythonIO.h
+++ Output/Output/PythonIO.h
@@ -1,10 +1,15 @@
 // Copyright 2017 Jean-Marie Mirebeau, University Paris-Sud, CNRS, University Paris-Saclay
 // Distributed WITHOUT ANY WARRANTY. Licensed under the Apache License, Version 2.0, see http://www.apache.org/licenses/LICENSE-2.0
 
-#ifndef MathematicaIO_h
-#define MathematicaIO_h
+#ifndef PythonIO_h
+#define PythonIO_h
 
 #include "BaseIO.h"
+
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+namespace py = pybind11;
+
 
 /*
 A simple interface for making Python data available to the c++ code.
@@ -13,15 +18,18 @@ A simple interface for making Python data available to the c++ code.
 struct PythonIO : BaseIO {
     // An adaptation of the file IO:
     PythonIO(){arrayOrdering = ArrayOrdering::YXZ_RowMajor;};
-    typedef boost::python::numpy::ndarray ndarray;
+    typedef py::array_t<ScalarType, py::array::c_style | py::array::forcecast > ndarray;
     ndarray PyGetArray(KeyCRef) const;
     void PySetArray(KeyCRef, ndarray);
     std::string GetComputedKeys() const;
 protected:
-    typedef boost::python::numpy::dtype dtype;
+    static void StaticSendMsg(bool warn, std::string msg){
+        py::print(warn ? ("Warning : "+msg) : msg);}
+    void SendMsg(bool warn, const std::string & msg) const {return StaticSendMsg(warn,msg);}
+    template<bool,typename> friend struct _Msg;
 };
 
 
 #include "PythonIO.hxx"
 
-#endif /* MathematicaIO_h */
+#endif /* PythonIO_h */
--- Output/Output/PythonIO.hxx
+++ Output/Output/PythonIO.hxx
@@ -1,31 +1,24 @@
-// Copyright 2017 Jean-Marie Mirebeau, University Paris-Sud, CNRS, University Paris-Saclay
+// Copyright 2018 Jean-Marie Mirebeau, University Paris-Sud, CNRS, University Paris-Saclay
 // Distributed WITHOUT ANY WARRANTY. Licensed under the Apache License, Version 2.0, see http://www.apache.org/licenses/LICENSE-2.0
 
 #ifndef PythonIO_hxx
 #define PythonIO_hxx
 
 void PythonIO::PySetArray(KeyCRef key, ndarray arr) {
-    if(arr.get_dtype() != dtype::get_builtin<double>())
-        ExceptionMacro("PythonIO error : ndarray " << key << " is not made of doubles");
     
+    py::buffer_info info = arr.request();
     RawElement & raw = CreateElement(key);
     raw.Clear(currentSetter);
-    const int ndims = arr.get_nd();
+    const int ndims = arr.ndim();
     raw.dims.resize(ndims);
-    DiscreteType stride = sizeof(ScalarType);
-    
-    for(int i=ndims-1; i>=0; --i){ // Row major
-        if(arr.strides(i)!=stride){   //TODO : Support arbitrary strides.
-            ExceptionMacro("PythonIO error : Sorry, only canonical strides are supported.");}
-        stride*=arr.shape(i);
-    }
+
     for(int i=0; i<ndims; ++i)
-        raw.dims[i]=arr.shape(i);
+        raw.dims[i]=info.shape[i];
     
     const DiscreteType flattenedLength = raw.FlattenedLength();
     raw.data.resize(flattenedLength);
     
-    ScalarType * beg = reinterpret_cast<ScalarType*>(arr.get_data());
+    ScalarType * beg = reinterpret_cast<ScalarType*>(info.ptr);
     std::copy(beg,beg+flattenedLength, raw.data.begin());
 }
 
@@ -33,17 +26,10 @@ auto PythonIO::PyGetArray(KeyCRef key) const -> ndarray {
     const RawElement & raw = GetRaw(key);
     if(raw.IsString()) {ExceptionMacro("PythonIO error : field " << key << " is a string, not an array");}
     if(raw.dims.empty()){ExceptionMacro("PythonIO error : field " << key << " is a scalar, not an array");}
-    boost::python::tuple shape;
-    for(DiscreteType dim : raw.dims){
-        shape = boost::python::extract<boost::python::tuple>(shape+boost::python::make_tuple(dim));} // Row major
-    ndarray result = boost::python::numpy::zeros(shape, dtype::get_builtin<double>());
+    ndarray result(raw.dims);
     
-/*    namespace p=boost::python;
-    std::cout << p::extract<char const *>(p::str(shape)) << std::endl;
-    std::cout << p::extract<char const *>(p::str(result)) << std::endl;*/
-
     assert(raw.FlattenedLength()==raw.data.size());
-    ScalarType * beg = reinterpret_cast<ScalarType*>(result.get_data());
+    ScalarType * beg = static_cast<ScalarType*>(result.request().ptr);
     std::copy(raw.data.begin(),raw.data.end(),beg);
     return result;
 }
